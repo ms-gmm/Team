92b9deff678149276d440ef1be441687f14a09e3
葛曼曼 <1151921639@qq.com>
2020-06-01 18:09:38 +0800

    添加项目

---
+++ Blog.Core/Blogs.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Blog.Core
+{
+   public class Blogs
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int Prasie { get; set; }
+
+        public string Comments { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ Blog.Core/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Blog.Core
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Education { get; set; }
+    }
+}
